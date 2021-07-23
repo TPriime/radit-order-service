10620e7d0957aee807d47c2ff6427f13803d562a
Anthony O. Agbafune <tonypriime@gmail.com>
2021-07-23 15:13:39 +0100

    disabled grpc

--- interface/http/middlewares/order.js
+++ interface/http/middlewares/order.js
@@ -2,8 +2,8 @@ const Errors = require("../utils/constants").errors;
 const Validators = require("../utils/validators");
 const productClient = require("../../rpc/clients/productClient");
 
-function _sendError(message, error, res) {
-  return res.status(400).json({
+function _sendError(message, error, res, status) {
+  return res.status(status || 400).json({
     status: Errors.FAILED,
     message: message,
     error: error,
@@ -16,17 +16,18 @@ function _sendError(message, error, res) {
   - and if products exist
 */
 module.exports.validateOrder = async (req, res, next) => {
-    const { error } = Validators.orderValidation(req.body);
-    if (error) return _sendError(error.details[0].message, error, res);
+  const { error } = Validators.orderValidation(req.body);
+  if (error) return _sendError(error.details[0].message, error, res);
 
-    // check for non existing product id
-    for(productId in req.body.productIds) {
-        try {
-          const available = (await productClient.getProductById(productId)).status;
-          if(!available) return _sendError(Errors.PRODUCT_NOT_EXISTS, error, res);
-        } catch (err) {
-          _sendError(Errors.INTERNAL_SERVER_ERROR, err, res);
-        }
+  // check for non existing product id
+  try {
+    for (productId in req.body.productIds) {
+      // const available = (await productClient.getProductById(productId)).status; TODO: fix rpc connection
+      // if (!available) return _sendError(Errors.PRODUCT_NOT_EXISTS, error, res);
     }
-    next();
+  } catch (err) {
+    console.log(`error connecting to client: ${err}`);
+    return _sendError(Errors.INTERNAL_SERVER_ERROR, err, res, 500);
+  }
+  next();
 };
